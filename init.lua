fcd039e0b204790ec297577ff3b5e45d7f5c2e6b
upsilon <upsilon@langg.net>
2017-02-11 19:07:48 +0100

    Allow to take items from autocrafters

--- init.lua
+++ init.lua
@@ -44,6 +44,7 @@ local containers = {
 	{"bottom", "default:furnace_active", "src"},
 	{"side", "default:furnace_active", "fuel"},
 
+	{"top", "autocrafter:autocrafter", "dst"},
 	{"bottom", "autocrafter:autocrafter", "src"},
 	{"side", "autocrafter:autocrafter", "src"},
 
